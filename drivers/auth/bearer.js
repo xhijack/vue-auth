5721a06a1a894dbbd2b0e02fbde403b877d0df01
ramdani <xhijack@gmail.com>
2018-07-28 15:48:36 +0700

    custom bearer

--- drivers/auth/bearer.js
+++ drivers/auth/bearer.js
@@ -5,13 +5,6 @@ module.exports = {
     },
     
     response: function (res) {
-        var headers = this.options.http._getHeaders.call(this, res),
-            token = headers.Authorization || headers.authorization;
-
-        if (token) {
-            token = token.split(/Bearer\:?\s?/i);
-            
-            return token[token.length > 1 ? 1 : 0].trim();
-        }
+		return res.data.token
     }
 };
